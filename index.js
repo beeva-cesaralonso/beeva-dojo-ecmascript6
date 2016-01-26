26b203a2c44a3dfa1875263a4593cb9d26533f04
César <cesar.alonso@beeva.com>
2016-01-26 12:09:58 +0100

    Unstable version

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const long = 10;
 
-var long = 10;
 var Utils = require(__dirname+'/lib/Utils');
 var Arrays = Utils.prototype.Arrays(); // llamamos a función estática de Utils que nos devuelve la clase Arrays
+
+
 var ua = new Arrays(long); // llamamos a clase hija de Utils para arrays
 var randomData = ua.getRandom(); // array de 10 els random
 console.log(randomData);
