1d0182bf0934e2a1ae9880151e16f257bb07beef
César <cesar.alonso@beeva.com>
2016-01-26 13:15:16 +0100

    First stable version, still have to improve it

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
 
 const long = 10;
-
-var Utils = require(__dirname+'/lib/Utils');
-var Arrays = Utils.prototype.Arrays(); // llamamos a función estática de Utils que nos devuelve la clase Arrays
+const Utils = require(__dirname+'/lib/Utils');
+const Arrays = Utils.arrays(); // llamamos a función estática de Utils que nos devuelve la clase Arrays
 
 
 var ua = new Arrays(long); // llamamos a clase hija de Utils para arrays
@@ -17,7 +16,7 @@ console.log(object.arr); // el array pasado
 console.log(object.list()); // el array en la clase
 console.log(object.getItem(5));
 object = object.getItem(5);
-console.log("El objeto con índice "+object.index+" vale "+object.value);
+console.log(`El objeto con índice ${object.index} vale ${object.value}`);
 
 
 var Weather = Utils.Weather(); // llamamos a función estática de Utils que nos devuelve la clase Weather
